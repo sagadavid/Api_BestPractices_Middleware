cc33943df8be85a8bbcff217263613e5d5008528
sagadavid <epostsaga@gmail.com>
2023-06-14 10:31:08 +0200

    terminal middleware with a status code, after next invoke

--- Api_BestPractices_Middleware/Program.cs
+++ Api_BestPractices_Middleware/Program.cs
@@ -12,6 +12,22 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.Use(async(context, next) =>
+{
+    Console.WriteLine("before- next delegate- use method");
+    await next.Invoke();
+    Console.WriteLine("after- next delegate- use method");
+});
+
+//terminal middleware, accepts only httpcontext parameter
+app.Run(async context =>
+{
+    Console.WriteLine($"response to the client- run method");
+    context.Response.StatusCode = 200;
+    await context.Response.WriteAsync("this is middleware component !");
+}
+);
+
 app.MapControllers();
 
 app.Run();
